767b0356a35024901e2becc0365acd216bdd3909
Hannah <hunter.hannah@icloud.com>
2015-04-26 10:26:37 -0500

    added user model and validation test

---
+++ test/models/user_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: "Test User", email: "test@user.com")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+end
